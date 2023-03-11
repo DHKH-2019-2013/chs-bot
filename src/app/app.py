485c3107c05926366612f56ad07fbbf860d1b2a7
nipponwfm <nipponwfm@gmail.com>
2023-03-11 13:28:22 +0700

    [feat] remove bot prefix in api

--- src/app/app.py
+++ src/app/app.py
@@ -5,7 +5,7 @@ from controllers.health_check import health_check
 
 app = Flask(__name__)
 
-app.register_blueprint(bot, url_prefix="/bot")
+app.register_blueprint(bot)
 app.register_blueprint(health_check)
 
 app.run(host=AppConfig.host, port=AppConfig.port)
